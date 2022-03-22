a93c75383cb2e310d4c9eb7d7223ad33597fdb7e
77den <77denisov@gmail.com>
2022-03-22 09:56:13 +0300

    creating a button and creating OnClickListener

--- app/src/main/java/ru/dw/gbkotlinmovieseach/MainActivity.kt
+++ app/src/main/java/ru/dw/gbkotlinmovieseach/MainActivity.kt
@@ -2,11 +2,28 @@ package ru.dw.gbkotlinmovieseach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.dw.gbkotlinmovieseach.R
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        onClickTestButton()
+    }
+
+    private fun onClickTestButton() {
+       button.setOnClickListener {
+           Toast.makeText(this, getString(R.string.test_info_button), Toast.LENGTH_SHORT).show()
+       }
+    }
+
+    private fun initView() {
+        button = findViewById(R.id.main_activity_test_button)
     }
 }
